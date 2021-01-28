58447ad4692fdd7347482e5dc4ee6b1fc79c0cba
Georg Ringer <georgringer@users.noreply.github.com>
2021-01-28 13:59:58 +0100

    Update ext_localconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,6 +3,6 @@
 $rendererRegistry = \TYPO3\CMS\Core\Resource\Rendering\RendererRegistry::getInstance();
 $rendererRegistry->registerRendererClass(\StudioMitte\Thinglink\Rendering\ThingLinkRenderer::class);
 
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['fal']['onlineMediaHelpers']['thinglink'] = \StudioMitte\Thinglink\Helpers\ThinglinkHelper::class;
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['fal']['onlineMediaHelpers']['thinglink'] = \StudioMitte\Thinglink\OnlineMedia\Helpers\ThinglinkHelper::class;
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['FileInfo']['fileExtensionToMimeType']['thinglink'] = 'image/thinglink';
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['mediafile_ext'] .= ',thinglink';
