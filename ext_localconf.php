621b7bb0965558e40943fc090c472e1457d0ce99
Rafal Jania <rj@studiomitte.com>
2023-08-23 09:39:59 +0200

    [WIP] Add TYPO3 v12 support

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,6 +1,6 @@
 <?php
 
-$rendererRegistry = \TYPO3\CMS\Core\Resource\Rendering\RendererRegistry::getInstance();
+$rendererRegistry = TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(TYPO3\CMS\Core\Resource\Rendering\RendererRegistry::class);
 $rendererRegistry->registerRendererClass(\StudioMitte\Thinglink\Rendering\ThingLinkRenderer::class);
 
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['fal']['onlineMediaHelpers']['thinglink'] = \StudioMitte\Thinglink\OnlineMedia\Helpers\ThinglinkHelper::class;
